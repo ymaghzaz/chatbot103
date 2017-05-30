c13713c8e174e3b6e04f369b4ac2462ccd56cf14
Florian Barbato <flobarbato@gmail.com>
2017-05-30 18:20:20 +0200

    deleted if

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,10 +9,7 @@ export default async function handleAction(res, payload) {
   const currentAction = res.action && res.action.slug
   console.log(currentAction)
   let replies = []
-  if (actions[currentAction]) {
-    console.log('Enter action')
-    replies = await actions[currentAction].default(res, payload)
-  } else if (res.reply()) {
+  if (res.reply()) {
     replies.push({
       type: 'text',
       content: res.reply(),
