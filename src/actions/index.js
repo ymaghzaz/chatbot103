ea9ada274609cb6a08f5d76c4e935aa8f9a45f5c
Youssef Maghzaz <youssef.maghzaz@co-assist.fr>
2017-05-30 20:54:21 +0200

    index add if action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,29 @@
 const actions = {
-  'trending-stars': require('./trending-stars'),
-  'trending-forks': require('./trending-forks'),
-  'show-language': require('./show-languages'),
-  'laugh': require('./laugh'),
+    'trending-stars': require('./trending-stars'),
+    'trending-forks': require('./trending-forks'),
+    'show-language': require('./show-languages'),
+    'laugh': require('./laugh'),
 }
 
-export default async function handleAction(res, payload) {
-  const currentAction = res.action && res.action.slug
-  console.log(currentAction)
-  let replies = []
-  if (res.reply()) {
-    replies.push({
-      type: 'text',
-      content: res.reply(),
-    })
-  } else {
-    replies.push({
-      type: 'text',
-      content: 'Sorry I did not understand',
-    })
-  }
-  return replies
+export default async
+function handleAction(res, payload) {
+    const currentAction = res.action && res.action.slug
+    console.log(currentAction)
+    let replies = []
+    if (actions[currentAction]) {
+        console.log('Enter action')
+        replies = await actions[currentAction].default(res, payload)
+    }
+   else  if (res.reply()) {
+        replies.push({
+            type: 'text',
+            content: res.reply(),
+        })
+    } else {
+        replies.push({
+            type: 'text',
+            content: 'Sorry I did not understand',
+        })
+    }
+    return replies
 }
