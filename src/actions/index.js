e4a0e3c435054e6860f543a0e667beacaee95cda
Youssef Maghzaz <youssef.maghzaz@co-assist.fr>
2017-05-30 21:07:32 +0200

    add most starred & forked

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 const actions = {
-    'trending-stars': require('./trending-stars'),
-    'trending-forks': require('./trending-forks'),
+    'most-starred': require('./most-starred'),
+    'most-forked': require('./most-forked'),
     'show-language': require('./show-languages'),
     'laugh': require('./laugh'),
 }
